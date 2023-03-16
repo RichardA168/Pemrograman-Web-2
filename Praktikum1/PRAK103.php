856185ae7eff6dd41536ee5fce533da1a45ac797
RichardA168 <104208649+RichardA168@users.noreply.github.com>
2023-03-16 19:52:18 +0800

    Update PRAK103.php

--- Praktikum1/PRAK103.php
+++ Praktikum1/PRAK103.php
@@ -12,9 +12,9 @@
     $F = round($C * 9/5 + 32, 4); //Celcius to Fahrenheit
     $R = round($C * 4/5, 4); //Celcius to Reamur
     $K = round($C + 273.15, 4); //Celcius to Kelvin
-    echo "$F <br>";
-    echo "$R <br>";
-    echo "$K";
+    echo "Fahrenheit (F) = $F <br>";
+    echo "Reamur (R) = $R <br>";
+    echo "Kelvin (K) = $K";
     ?>
 </body>
 </html>
