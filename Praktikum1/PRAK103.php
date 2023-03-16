304f61f5bc399cf184fcefdd018ab87a5a74e284
RichardA168 <104208649+RichardA168@users.noreply.github.com>
2023-03-16 19:58:43 +0800

    Update PRAK103.php

--- Praktikum1/PRAK103.php
+++ Praktikum1/PRAK103.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PRAK103</title>
-</head>
-<body>
-    <?php
+<?php
     $C = 37.841; //Celcius
     $F = round($C * 9/5 + 32, 4); //Celcius to Fahrenheit
     $R = round($C * 4/5, 4); //Celcius to Reamur
@@ -15,6 +6,4 @@
     echo "Fahrenheit (F) = $F <br>";
     echo "Reamur (R) = $R <br>";
     echo "Kelvin (K) = $K";
-    ?>
-</body>
-</html>
+?>
